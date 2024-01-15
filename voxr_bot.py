c003f52b423cff0eeea344db8bc3930f7dd9737a
jacurlive <jasurkkirchh@gmail.com>
2024-01-15 14:10:39 +0500

    Add new function

--- voxr_bot.py
+++ voxr_bot.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import os
 
 import openpyxl
 import logging
@@ -53,6 +54,29 @@ async def get_contact(message: types.Message):
 
     await bot.send_message(message.chat.id, info_str)
     await bot.send_message(message.chat.id, status)
+
+
+@dp.message(F.document)
+async def admin_work(message: types.Message):
+    if message.chat.id == 5728980889:
+        await bot.send_message(5728980889, "Hi, admin")
+        
+        document = message.document
+
+        file_info = await bot.get_file(document.file_id)
+
+        file_path = await bot.download_file(file_info.file_path)
+
+        if os.path.exists("data.xlsx"):
+            os.remove("data.xlsx")
+
+        with open(f'data.xlsx', 'wb') as new_file:
+            new_file.write(file_path.read())
+
+        await bot.send_message(5728980889, "Файл сохранен успешно!")
+
+    else:
+        pass
 
 
 @dp.message()
