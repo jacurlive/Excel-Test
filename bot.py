d2d5d80a7a8c64b8e42f0ed554454ec9bbc4caa3
jacurlive <jasurkkirchh@gmail.com>
2024-01-20 11:14:43 +0500

    add all functions to utils

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from utils.functions import add_plus
+from utils.functions import add_plus, get_salary, add_user, save_file
 from utils.database import Database
 
 load_dotenv()
@@ -26,41 +26,18 @@ reply_keyboard = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="Raqamni Yub
 
 @dp.message(CommandStart())
 async def start(message: types.Message):
-    if not db.user_exists(message.from_user.id):
-        db.add_user(message.from_user.id, message.from_user.first_name, message.from_user.last_name)
+    add_user(message.from_user.id, message.from_user.first_name, message.from_user.last_name)
     await message.answer("Salom!Malumotni korish uchun pastdagi tugmani bosing!", reply_markup=reply_keyboard)
 
 
 @dp.message(F.contact)
 async def get_contact(message: types.Message):
-    if not db.user_exists(message.from_user.id):
-        db.add_user(message.from_user.id, message.from_user.first_name, message.from_user.last_name)
+    add_user(message.from_user.id, message.from_user.first_name, message.from_user.last_name)
     contact = message.contact
+    contact = add_plus(contact.phone_number)
     await message.answer("Raqam muvofaqiyatli royhatdan otdi!Hisob izlanmoqda...")
-    book = openpyxl.load_workbook("data/data.xlsx", read_only=True)
-    try:
-        sheet = book.active
-        status = "Hisob topilmadi!"
-        info = {}
-        await bot.send_message(819233688,
-                               f"name: {contact.first_name}\nlastName: {contact.last_name}\nid: {contact.user_id}\n"
-                               f"phoneNumber: {contact.phone_number}")
-        for item in range(1, sheet.max_row + 1):
-            numb = add_plus(contact.phone_number)
-            if sheet[item][0].value == numb:
-                for item2 in range(0, sheet.max_column):
-                    if sheet[item][item2].value:
-                        info[sheet[3][item2].value] = sheet[item][item2].value
-                    else:
-                        continue
-                break
-        if not info:
-            await bot.send_message(message.chat.id, status)
-        else:
-            text = "\n".join([f"<b>{key}</b>:  {value}" for key, value in info.items()])
-            await bot.send_message(message.chat.id, text, parse_mode="html")
-    finally:
-        book.close()
+    info = get_salary(contact)
+    await bot.send_message(message.chat.id, info, parse_mode="html")
 
 
 @dp.message(F.document)
@@ -69,16 +46,11 @@ async def admin_work(message: types.Message):
         document = message.document
         file_info = await bot.get_file(document.file_id)
         file_path = await bot.download_file(file_info.file_path)
-        if os.path.exists("data/data.xlsx"):
-            try:
-                os.remove("data/data.xlsx")
-            except Exception as ex:
-                await bot.send_message(message.chat.id, f"error: {str(ex)}")
-        with open(f'data/data.xlsx', 'wb') as new_file:
-            new_file.write(file_path.read())
-        await bot.send_message(message.chat.id, "Файл сохранен успешно!")
+        info = save_file(file_path)
+        await bot.send_message(message.chat.id, info, parse_mode="html")
     else:
-        await bot.send_message(message.chat.id, "У вас нет прав для выполнения этой операции.")
+        await bot.send_message(message.chat.id, "<b>У вас нет прав для выполнения этой операции.</b>",
+                               parse_mode="html")
 
 
 @dp.message(Command("message"))
--- utils/functions.py
+++ utils/functions.py
@@ -1,5 +1,49 @@
+import openpyxl
+import os
+from utils.database import Database
+
+db = Database("data/db.sqlite3")
+
 
 def add_plus(number: str) -> str:
     if number.startswith("+"):
         return number
     return "+" + number
+
+
+def get_salary(number):
+    book = openpyxl.load_workbook("data/data.xlsx", read_only=True)
+    try:
+        sheet = book.active
+        info = {}
+        for item in range(1, sheet.max_row + 1):
+            if sheet[item][0].value == number:
+                for item2 in range(0, sheet.max_column):
+                    if sheet[item][item2].value:
+                        info[sheet[3][item2].value] = sheet[item][item2].value
+                    else:
+                        continue
+                break
+        if not info:
+            return "<b>Hisob topilmadi!</b>"
+        else:
+            text = "\n".join([f"<b>{key}</b>:  {value}" for key, value in info.items()])
+            return text
+    finally:
+        book.close()
+
+
+def add_user(userid, username, lastname):
+    if not db.user_exists(userid):
+        db.add_user(userid, username, lastname)
+
+
+def save_file(filepath):
+    if os.path.exists("data/data.xlsx"):
+        try:
+            os.remove("data/data.xlsx")
+        except Exception as ex:
+            return f"<b>error: {str(ex)}</b>"
+    with open(f'data/data.xlsx', 'wb') as new_file:
+        new_file.write(filepath.read())
+        return "<b>Файл сохранен успешно!</b>"
